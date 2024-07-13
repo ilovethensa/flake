e979913a86d83fbfac862d368d032a12ac9bd460
Bill Gates <me@theholytachanka.com>
2024-07-13 22:30:50 +0300

    just work

--- nix/options/gpu/intel.nix
+++ nix/options/gpu/intel.nix
@@ -15,12 +15,21 @@ in {
   };
 
   config = lib.mkIf cfg.intel {
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    };
     hardware.graphics = {
       # hardware.opengl in 24.05
       enable = true;
       extraPackages = with pkgs; [
         intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
         vpl-gpu-rt # or intel-media-sdk for QSV
+        intel-vaapi-driver # previously vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+        vpl-gpu-rt # QSV on 11th gen or newer
+        intel-media-sdk # QSV up to 11th gen
       ];
     };
     environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Optionally, set the environment variable
