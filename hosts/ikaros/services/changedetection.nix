2ae11e0c4ba158efbe8d99f9f86083dbfa884cb9
Bill Gates <me@theholytachanka.com>
2024-07-14 15:18:54 +0300

    added changedetection

--- hosts/ikaros/services/changedetection.nix
+++ hosts/ikaros/services/changedetection.nix
@@ -1,41 +1,9 @@
 {...}: {
-  virtualisation.oci-containers.containers = {
-    changedetection-playwright = {
-      image = "browserless/chrome";
-      environment = {
-        SCREEN_WIDTH = "1920";
-        SCREEN_HEIGHT = "1024";
-        SCREEN_DEPTH = "16";
-        ENABLE_DEBUGGER = "false";
-        PREBOOT_CHROME = "true";
-        CONNECTION_TIMEOUT = "300000";
-        MAX_CONCURRENT_SESSIONS = "10";
-        CHROME_REFRESH_TIME = "600000";
-        DEFAULT_BLOCK_ADS = "true";
-        DEFAULT_STEALTH = "true";
-        HOST = "192.168.1.111";
-      };
-      ports = [
-        "3000:3000"
-      ];
-      extraOptions = ["--network=bridge"];
-    };
-    changedetection = {
-      image = "ghcr.io/dgtlmoon/changedetection.io:0.45.24";
-      environment = {
-        PLAYWRIGHT_DRIVER_URL = "ws://192.168.1.111:3000";
-      };
-      ports = [
-        "5000:5000"
-      ];
-      extraOptions = ["--network=bridge"];
-      volumes = [
-        "/mnt/data/changedetection:/datastore"
-      ];
-      dependsOn = [
-        "changedetection-playwright"
-      ];
-    };
+  services.changedetection-io = {
+    enable = true;
+    playwrightSupport = true;
+    port = 5000;
+    chromePort = 3000;
   };
 
   networking.firewall.allowedTCPPorts = [
