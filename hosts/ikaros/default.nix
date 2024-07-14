823235a386160fbd8ac5e7ba70262a67aa4f179e
Bill Gates <me@theholytachanka.com>
2024-07-14 11:31:01 +0300

    just work please

--- hosts/ikaros/default.nix
+++ hosts/ikaros/default.nix
@@ -33,13 +33,31 @@
     };
   };
   meta.intel = true;
-  users.users.root = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1TqzsM18I70A0XhmBjYhryhkRFdGl6ofup92N3XNyC tht" # content of authorized_keys file
-      # note: ssh-copy-id will add user@your-machine after the public key
-      # but we can remove the "@your-machine" part
-    ];
-    initialHashedPassword = "$6$dO.lQ2bJZ5PBNhju$WXDIMEWtKFSTw94iRX7Pi2HwsN6hw2q4k3D6UxJXUdMm1VsrtrqpbC/4iyiOBhU.fxWKkugP30s.RuK6IYbWN.";
+  programs.fish.enable = true;
+  users = {
+    defaultUserShell = pkgs.fish;
+    mutableUsers = false;
+    users = {
+      root = {
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1TqzsM18I70A0XhmBjYhryhkRFdGl6ofup92N3XNyC tht" # content of authorized_keys file
+          # note: ssh-copy-id will add user@your-machine after the public key
+          # but we can remove the "@your-machine" part
+        ];
+        initialHashedPassword = "$6$dO.lQ2bJZ5PBNhju$WXDIMEWtKFSTw94iRX7Pi2HwsN6hw2q4k3D6UxJXUdMm1VsrtrqpbC/4iyiOBhU.fxWKkugP30s.RuK6IYbWN.";
+      };
+      tht = {
+        isNormalUser = true;
+        extraGroups = ["wheel" "render" "video" "docker" "adbusers" "gamemode"]; # Enable ‘sudo’ for the user.
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1TqzsM18I70A0XhmBjYhryhkRFdGl6ofup92N3XNyC tht" # content of authorized_keys file
+          # note: ssh-copy-id will add user@your-machine after the public key
+          # but we can remove the "@your-machine" part
+        ];
+
+        initialHashedPassword = "$6$dO.lQ2bJZ5PBNhju$WXDIMEWtKFSTw94iRX7Pi2HwsN6hw2q4k3D6UxJXUdMm1VsrtrqpbC/4iyiOBhU.fxWKkugP30s.RuK6IYbWN.";
+      };
+    };
   };
   #networking.wireguard.enable = true;
   # Persistence configuration
--- nix/services/openssh.nix
+++ nix/services/openssh.nix
@@ -1,13 +1,9 @@
-{lib, ...}: {
+{...}: {
   services.openssh = {
     enable = true;
     # require public key authentication for better security
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
     settings.PermitRootLogin = "yes";
-  };
-  environment.etc = {
-    "ssh/sshd_config".text = lib.mkForce ''
-    '';
   };
 }
