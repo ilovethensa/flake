84aa7d053c4b71e58dce0088d69262d89221f08b
Bill Gates <me@theholytachanka.com>
2024-07-16 12:57:07 +0300

    added some ssh logs analyzing

--- hosts/ikaros/services/cron.nix
+++ hosts/ikaros/services/cron.nix
@@ -5,11 +5,17 @@
       systemctl restart "$unit"
     done
   '';
+  ssh-logs = pkgs.writeShellScriptBin "ssh-logs.sh" ''
+mkdir /var/stats
+{ echo "root $(grep -c 'Connection closed by authenticating user root' <(journalctl -xeu sshd))"; grep 'Invalid user' <(journalctl -xeu sshd) | awk '{print $8}' | sort | uniq -c | awk '{print $2, $1}'; } | sort -k2,2nr > /var/stats/users.txt
+journalctl -xeu sshd | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -vE '^192\.' | sort | uniq -c | sort -nr | awk '{print $2, $1}' > /var/stats/ips.txt
+  '';
 in {
   services.cron = {
     enable = true;
     systemCronJobs = [
       "0 * /4 * * * ${restart-podman}"
+      "0 * /4 * * * ${ssh-logs}"
     ];
   };
 }
