13aec056d2c8dea099f7cf03deae505635772246
Bill Gates <me@theholytachanka.com>
2024-07-17 08:29:41 +0300

    fix maybe

--- hosts/ikaros/services/tor.nix
+++ hosts/ikaros/services/tor.nix
@@ -1,40 +1,41 @@
 {...}: {
-services.tor = {
-  enable = true; 
-
-  # Disable GeoIP to prevent the Tor client from estimating the locations of Tor nodes it connects to
-  enableGeoIP = false;
-
-  # Enable and configure the Tor relay
-  relay = {
+  services.tor = {
     enable = true;
-    role = "relay";  # Set the relay role (e.g., "relay", "bridge")
+
+    # Disable GeoIP to prevent the Tor client from estimating the locations of Tor nodes it connects to
+    enableGeoIP = false;
+
+    # Enable and configure the Tor relay
+    relay = {
+      enable = true;
+      role = "relay"; # Set the relay role (e.g., "relay", "bridge")
+    };
+    openFirewall = true;
+    # Configure Tor settings
+    settings = {
+      Nickname = "tht";
+      ContactInfo = "me@theholytachanka.com";
+
+      # Bandwidth settings
+      MaxAdvertisedBandwidth = "5 MB";
+      BandWidthRate = "3 MB";
+      RelayBandwidthRate = "3 MB";
+      RelayBandwidthBurst = "5 MB";
+
+      # Reject all exit traffic
+      ExitPolicy = "reject *:*";
+
+      # Performance and security settings
+      CookieAuthentication = true;
+      AvoidDiskWrites = 1;
+      HardwareAccel = 1;
+      SafeLogging = 1;
+      NumCPUs = 2;
+
+      Address = "91.139.255.24";
+
+      # Network settings
+      ORPort = [456];
+    };
   };
-  openFirewall = true;
-  # Configure Tor settings
-  settings = {
-    Nickname = "tht";  
-    ContactInfo = "me@theholytachanka.com"; 
-
-    # Bandwidth settings
-    MaxAdvertisedBandwidth = "5 MB";  
-    BandWidthRate = "3 MB";  
-    RelayBandwidthRate = "3 MB";  
-    RelayBandwidthBurst = "5 MB"; 
-
-    # Reject all exit traffic
-    ExitPolicy = "reject *:*";  
-
-    # Performance and security settings
-    CookieAuthentication = true;  
-    AvoidDiskWrites = 1; 
-    HardwareAccel = 1;  
-    SafeLogging = 1; 
-    NumCPUs = 2;   
-
-    # Network settings
-    ORPort = [456];
-  };
-};
-
 }
