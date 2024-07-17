88c5b5aa2f7dae7bebfe1b4add2a002e9b54c804
Bill Gates <me@theholytachanka.com>
2024-07-17 14:48:08 +0300

    made foot use gruvbox

--- home/tht/default.nix
+++ home/tht/default.nix
@@ -1,10 +1,6 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
