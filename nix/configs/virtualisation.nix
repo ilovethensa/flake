3f73e6e29dee1efd618608de9e4d8c3d068b4389
Bill Gates <me@theholytachanka.com>
2024-07-23 19:57:03 +0300

    pls work

--- nix/configs/virtualisation.nix
+++ nix/configs/virtualisation.nix
@@ -1,4 +1,12 @@
 {pkgs, ...}: {
+  ennvironment.systemPackages = with pkgs; [
+    qemu
+    (pkgs.writeShellScriptBin "qemu-system-x86_64-uefi" ''
+      qemu-system-x86_64 \
+        -bios ${pkgs.OVMF.fd}/FV/OVMF.fd \
+        "$@"
+    '')
+  ];
   programs.virt-manager.enable = true;
   virtualisation.libvirtd = {
     enable = true;
