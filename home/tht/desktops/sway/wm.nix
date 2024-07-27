f7c36831309bc407f7af4f11a3aa8ee63cdd515c
Bill Gates <me@theholytachanka.com>
2024-07-27 18:11:09 +0300

    added keybinds

--- home/tht/desktops/sway/wm.nix
+++ home/tht/desktops/sway/wm.nix
@@ -41,6 +41,13 @@
         "${modifier}+Shift+Down" = "move down";
         "${modifier}+Shift+Up" = "move up";
         "${modifier}+Shift+Right" = "move right";
+        # Brightness
+        "XF86MonBrightnessDown" = "exec light -U 10";
+        "XF86MonBrightnessUp" = "exec light -A 10";
+        # Audio
+        "XF86AudioRaiseVolume" = "exec '${pkgs.ulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +1%'";
+        "XF86AudioLowerVolume" = "exec '${pkgs.ulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -1%'";
+        "XF86AudioMute" = "exec '${pkgs.ulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle'";
       };
       startup = [
         {command = "${pkgs.gammastep}/bin/gammastep -o -O 2000";}
