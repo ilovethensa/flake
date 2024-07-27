d162d02dd2a61d1ab464a016d513ef4ad66f71e0
Bill Gates <me@theholytachanka.com>
2024-07-27 12:49:29 +0300

    added some stuff

--- home/tht/apps/qutebrowser/default.nix
+++ home/tht/apps/qutebrowser/default.nix
@@ -19,9 +19,24 @@
         url = "https://update.greasyfork.org/scripts/466058/Dark%20Reader.user.js";
         sha256 = "sha256-lkZksw4tZcA3jTldcivzh3TDMKqcA38u9sbE3qklQP8=";
       })
+      (pkgs.fetchurl {
+        url = "https://update.greasyfork.org/scripts/453320/Simple%20Sponsor%20Skipper.user.js";
+        sha256 = "sha256-SmdyGmMVEQYV9sQUjwxq8OkJ+7BBqWpkRyNoL5xqiuQ=";
+      })
+      (pkgs.fetchurl {
+        url = "https://update.greasyfork.org/scripts/465936/Proxy%20Redirect.user.js";
+        sha256 = "sha256-YgSPdCaFqYkk6Y0/ifuDk0hJtFmQPxDBCUdx8KjyQM0=";
+      })
+      (pkgs.fetchurl {
+        url = "https://update.greasyfork.org/scripts/491832/Remove%20Share%20Buttons.user.js";
+        sha256 = "sha256-xp+sbM+rcRQ5d3WXm/t+q3fO8GI0Pe0duXC/o3XbwE8=";
+      })
     ];
     settings = {
       colors.webpage.preferred_color_scheme = "dark";
+      colors.webpage.darkmode.enabled = true;
+      auto_save.session = true;
+      content.blocking.method = "both";
     };
     extraConfig = ''
       import catppuccin
