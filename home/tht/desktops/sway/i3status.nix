5793e6779362230868f54e4cef25d00bf5c45160
Bill Gates <me@theholytachanka.com>
2024-07-27 19:55:19 +0300

    tweaks to the bar

--- home/tht/desktops/sway/i3status.nix
+++ home/tht/desktops/sway/i3status.nix
@@ -1,5 +1,32 @@
 {...}: {
   programs.i3status = {
     enable = true;
+    modules = {
+      "wireless _first_" = {
+        format_down = "W: down";
+        format_up = "W: (%quality at %essid) %ip";
+      };
+      "ethernet _first_" = {
+        format_down = "E: down";
+        format_up = "E: %ip (%speed)";
+      };
+      "battery all" = {
+        format = "%status %percentage %remaining";
+      };
+      "memory" = {
+        format = "%used | %available";
+        format_degraded = "MEMORY < %available";
+        threshold_degraded = "1G";
+      };
+      "disk /nix" = {
+        format = "%avail";
+      };
+      "load" = {
+        format = "%1min";
+      };
+      "tztime local" = {
+        format = "%Y-%m-%d %H:%M:%S";
+      };
+    };
   };
 }
