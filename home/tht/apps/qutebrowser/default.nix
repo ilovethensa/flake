430facf29595d7ff6a41e419a97d8e23caf3d28e
Bill Gates <me@theholytachanka.com>
2024-07-27 10:30:41 +0300

    added dark reader

--- home/tht/apps/qutebrowser/default.nix
+++ home/tht/apps/qutebrowser/default.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   home.file.".config/qutebrowser/catppuccin".source = inputs.qutebrowser-catppucin;
   programs.qutebrowser = {
     enable = true;
@@ -8,6 +12,15 @@
       nw = "https://wiki.nixos.org/index.php?search={}";
       ns = "https://mynixos.com/search?q={}";
       np = "https://search.nixos.org/packages?query={}";
+    };
+    greasemonkey = [
+      (pkgs.fetchurl {
+        url = "https://update.greasyfork.org/scripts/466058/Dark%20Reader.user.js";
+        sha256 = "sha256-lkZksw4tZcA3jTldcivzh3TDMKqcA38u9sbE3qklQP8=";
+      })
+    ];
+    settings = {
+      colors.webpage.preferred_color_scheme = "dark";
     };
     extraConfig = ''
       import catppuccin
