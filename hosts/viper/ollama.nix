ad1392c2c217dfc34c22649082ae26ed425f7822
Bill Gates <me@theholytachanka.com>
2024-07-28 13:34:46 +0300

    oops

--- hosts/viper/ollama.nix
+++ hosts/viper/ollama.nix
@@ -4,7 +4,7 @@
     acceleration = "rocm";
     environmentVariables = {
       HCC_AMDGPU_TARGET = "gfx803"; # used to be necessary, but doesn't seem to anymore
-      ROC_ENABLE_PRE_VEGA = 1;
+      ROC_ENABLE_PRE_VEGA = "1";
     };
     rocmOverrideGfx = "10.3.1";
   };
