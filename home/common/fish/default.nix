e3c418df860a56905147dabf9e38b315a03d0fa8
Bill Gates <me@theholytachanka.com>
2024-07-30 09:58:24 +0300

    added zoxide

--- home/common/fish/default.nix
+++ home/common/fish/default.nix
@@ -37,64 +37,65 @@ in {
     just
     wl-clipboard-rs
   ];
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-      ${pkgs.starship}/bin/starship init fish | source
-      ${pkgs.nitch}/bin/nitch
-      function help
-        ${pkgs.curl}/bin/curl cheat.sh/$argv | ${pkgs.less}/bin/less
-      end
-      function nix-init
-        set -l lang $argv[1]
-        set -l project_name $argv[2]
-
-        mkdir $project_name
-        cd $project_name
-        nix flake init -t ~/Projects/lol/dots/#$lang
-      end
-    '';
-    shellAliases = {
-      ls = "${pkgs.eza}/bin/eza -la";
-      cat = "${pkgs.bat}/bin/bat --paging=never";
-      cp = "${pkgs.xcp}/bin/xcp";
-      rm = "${pkgs.fuc}/bin/rmz";
-      glow = "${pkgs.glow}/bin/glow --pager";
-      htop = "${pkgs.bottom}/bin/btm --basic --tree --hide_table_gap --dot_marker --mem_as_value";
-      ip = "${pkgs.iproute2}/bin/ip --color --brief";
-      less = "${pkgs.bat}/bin/bat";
-      dmesg = "${pkgs.util-linux}/bin/dmesg --human --color=always";
-      tree = "${pkgs.eza}/bin/eza --tree";
-      ping = "${pkgs.gping}/bin/gping";
-      ask = "${pkgs.tgpt}/bin/tgpt";
-      backup-server = "${backup-server}/bin/backup-server.sh";
-      reboot = "systemctl reboot";
-      shutdown = "systemctl poweroff";
-    };
-    plugins = [
-      # Enable a plugin (here grc for colorized command output) from nixpkgs
-      {
-        name = "grc";
-        inherit (pkgs.fishPlugins.grc) src;
-      }
-      # Manually packaging and enable a plugin
-      {
-        name = "z";
-        src = pkgs.fetchFromGitHub {
-          owner = "jethrokuan";
-          repo = "z";
-          rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
-          sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
-        };
-      }
-    ];
-    /*
-           direnv = {
+  programs = {
+    zoxide = {
       enable = true;
-      enableFishIntegration = true; # see note on other shells below
-      nix-direnv.enable = true;
+      options = [
+        "--cmd cd"
+      ];
     };
-    */
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+        ${pkgs.starship}/bin/starship init fish | source
+        ${pkgs.nitch}/bin/nitch
+        function help
+          ${pkgs.curl}/bin/curl cheat.sh/$argv | ${pkgs.less}/bin/less
+        end
+        function nix-init
+          set -l lang $argv[1]
+          set -l project_name $argv[2]
+
+          mkdir $project_name
+          cd $project_name
+          nix flake init -t ~/Projects/lol/dots/#$lang
+        end
+      '';
+      shellAliases = {
+        ls = "${pkgs.eza}/bin/eza -la";
+        cat = "${pkgs.bat}/bin/bat --paging=never";
+        cp = "${pkgs.xcp}/bin/xcp";
+        rm = "${pkgs.fuc}/bin/rmz";
+        glow = "${pkgs.glow}/bin/glow --pager";
+        htop = "${pkgs.bottom}/bin/btm --basic --tree --hide_table_gap --dot_marker --mem_as_value";
+        ip = "${pkgs.iproute2}/bin/ip --color --brief";
+        less = "${pkgs.bat}/bin/bat";
+        dmesg = "${pkgs.util-linux}/bin/dmesg --human --color=always";
+        tree = "${pkgs.eza}/bin/eza --tree";
+        ping = "${pkgs.gping}/bin/gping";
+        ask = "${pkgs.tgpt}/bin/tgpt";
+        backup-server = "${backup-server}/bin/backup-server.sh";
+        reboot = "systemctl reboot";
+        shutdown = "systemctl poweroff";
+      };
+      plugins = [
+        # Enable a plugin (here grc for colorized command output) from nixpkgs
+        {
+          name = "grc";
+          inherit (pkgs.fishPlugins.grc) src;
+        }
+        # Manually packaging and enable a plugin
+        {
+          name = "z";
+          src = pkgs.fetchFromGitHub {
+            owner = "jethrokuan";
+            repo = "z";
+            rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+            sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+          };
+        }
+      ];
+    };
   };
 }
