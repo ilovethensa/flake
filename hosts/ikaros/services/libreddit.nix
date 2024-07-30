ccafc19835fb0c97dd2e09cd6cce462b00206c68
Bill Gates <me@theholytachanka.com>
2024-07-30 11:16:17 +0300

    made libreddit.nix better

--- hosts/ikaros/services/libreddit.nix
+++ hosts/ikaros/services/libreddit.nix
@@ -1,22 +1,22 @@
 {_}: {
-  services.libreddit = {
-    enable = true;
-    port = 5462;
-  };
-  services.caddy = {
-    enable = true;
-    virtualHosts = {
-      "lr.pwned.page".extraConfig = ''
-        reverse_proxy http://localhost:5462
-      '';
+  services = {
+    libreddit = {
+      enable = true;
+      port = 5462;
     };
-  };
-
-  services.cloudflare-dyndns.domains = [
-    "lr.pwned.page"
-  ];
-
-  security.acme.certs = {
-    "lr.pwned.page" = {};
+    caddy = {
+      enable = true;
+      virtualHosts = {
+        "lr.pwned.page".extraConfig = ''
+          reverse_proxy http://localhost:5462
+        '';
+      };
+    };
+    cloudflare-dyndns.domains = [
+      "lr.pwned.page"
+    ];
+    acme.certs = {
+      "lr.pwned.page" = {};
+    };
   };
 }
