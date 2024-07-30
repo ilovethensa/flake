2ff6cfe9c0012e6fcfbd5e3fe94631d0c73ad543
Bill Gates <me@theholytachanka.com>
2024-07-30 11:07:29 +0300

    made hyprland.nix better

--- nix/configs/desktop/hyprland.nix
+++ nix/configs/desktop/hyprland.nix
@@ -1,16 +1,24 @@
 {pkgs, ...}: {
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-        user = "greeter";
+  services = {
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+          user = "greeter";
+        };
       };
     };
+    # Enable the gnome-keyring secrets vault.
+    # Will be exposed through DBus to programs willing to store secrets.
+    gnome.gnome-keyring.enable = true;
+    xserver = {
+      enable = true;
+      excludePackages = [pkgs.xterm];
+      displayManager.startx.enable = true;
+    };
   };
-  # Enable the gnome-keyring secrets vault.
-  # Will be exposed through DBus to programs willing to store secrets.
-  services.gnome.gnome-keyring.enable = true;
+
   programs.hyprland = {
     # Install the packages from nixpkgs
     enable = true;
@@ -30,13 +38,7 @@
     };
   };
   security.polkit.enable = true;
-  services.xserver = {
-    enable = true;
-    excludePackages = [pkgs.xterm];
-    displayManager.startx.enable = true;
-  };
   fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["FiraCode"];})
   ];
 }
- 
