c2634d2e50e34e72323d89efc1a16cfdba0345a9
Bill Gates <me@theholytachanka.com>
2024-07-30 11:02:05 +0300

    made miniflux.nix better

--- hosts/ikaros/services/miniflux.nix
+++ hosts/ikaros/services/miniflux.nix
@@ -1,12 +1,16 @@
 # ADMIN_USERNAME=my_admin_username
 # ADMIN_PASSWORD=my_secure_password
 {config, ...}: {
-  sops.secrets.miniflux_user = {};
-  sops.secrets.miniflux_pass = {};
-  sops.templates."miniflux_env".content = ''
-    ADMIN_USERNAME="${config.sops.placeholder.miniflux_user}"
-    ADMIN_PASSWORD="${config.sops.placeholder.miniflux_pass}"
-  '';
+  sops = {
+    templates."miniflux_env".content = ''
+      ADMIN_USERNAME="${config.sops.placeholder.miniflux_user}"
+      ADMIN_PASSWORD="${config.sops.placeholder.miniflux_pass}"
+    '';
+    secrets = {
+      miniflux_pass = {};
+      miniflux_user = {};
+    };
+  };
   services.miniflux = {
     enable = true;
     adminCredentialsFile = config.sops.templates."miniflux_env".path;
