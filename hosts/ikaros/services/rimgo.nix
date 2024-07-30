336a02c3fec8b0b8287d046f6626973875092763
Bill Gates <me@theholytachanka.com>
2024-07-30 11:13:44 +0300

    made rimgo.nix better

--- hosts/ikaros/services/rimgo.nix
+++ hosts/ikaros/services/rimgo.nix
@@ -1,32 +1,32 @@
 {_}: {
-  services.rimgo = {
-    enable = true;
-    settings = {
-      ADDRESS = "0.0.0.0";
-      PORT = 3495;
-      PRIVACY_COUNTRY = "BG";
-      PRIVACY_PROVIDER = "ISP";
-      PRIVACY_NOT_COLLECTED = "1";
-      PRIVACY_IP = "1";
-      PRIVACY_URL = "1";
-      PRIVACY_DEVICE = "1";
-      PRIVACY_DIAGNOSTICS = "1";
+  services = {
+    rimgo = {
+      enable = true;
+      settings = {
+        ADDRESS = "0.0.0.0";
+        PORT = 3495;
+        PRIVACY_COUNTRY = "BG";
+        PRIVACY_PROVIDER = "ISP";
+        PRIVACY_NOT_COLLECTED = "1";
+        PRIVACY_IP = "1";
+        PRIVACY_URL = "1";
+        PRIVACY_DEVICE = "1";
+        PRIVACY_DIAGNOSTICS = "1";
+      };
     };
-  };
-  services.caddy = {
-    enable = true;
-    virtualHosts = {
-      "rimgo.pwned.page".extraConfig = ''
-        reverse_proxy http://localhost:3495
-      '';
+    caddy = {
+      enable = true;
+      virtualHosts = {
+        "rimgo.pwned.page".extraConfig = ''
+          reverse_proxy http://localhost:3495
+        '';
+      };
     };
-  };
-
-  services.cloudflare-dyndns.domains = [
-    "rimgo.pwned.page"
-  ];
-
-  security.acme.certs = {
-    "rimgo.pwned.page" = {};
+    cloudflare-dyndns.domains = [
+      "rimgo.pwned.page"
+    ];
+    acme.certs = {
+      "rimgo.pwned.page" = {};
+    };
   };
 }
