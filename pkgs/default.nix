b03fb72d4fd4a5fc121f6a2e846777c1763c3f6c
Bill Gates <me@theholytachanka.com>
2024-08-01 14:31:18 +0300

    fix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 {pkgs ? import <nixpkgs> {}, ...}: {
   # example = pkgs.callPackage ./example { };
-  prismlauncher-cracked-unwrapped = pkgs.libsForQt5.callPackage ./prismlauncher-cracked-unwrapped {};
-  prismlauncher-cracked- = pkgs.libsForQt5.callPackage ./prismlauncher-cracked {};
+  prismlauncher-cracked = pkgs.libsForQt5.callPackage ./prismlauncher-cracked {};
 }
