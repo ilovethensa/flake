6b7944c272c48532ee66729bed52390b04422e93
Bill Gates <me@theholytachanka.com>
2024-08-02 19:38:54 +0300

    added bat

--- home/tht/desktops/sway/bat.nix
+++ home/tht/desktops/sway/bat.nix
@@ -7,7 +7,7 @@
           owner = "catppuccin";
           repo = "bat";
           rev = "82e7ca555f805b53d2b377390e4ab38c20282e83";
-          sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+          sha256 = "sha256-/Ob9iCVyjJDBCXlss9KwFQTuxybmSSzYRBZxOT10PZg=";
         };
         file = "themes/Catppuccin Mocha.tmTheme";
       };
