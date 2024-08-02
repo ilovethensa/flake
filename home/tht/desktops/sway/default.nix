3b71ee83b1fd076cacb7170439589b52ee31d45e
Bill Gates <me@theholytachanka.com>
2024-08-02 14:52:12 +0300

    added theming

--- home/tht/desktops/sway/default.nix
+++ home/tht/desktops/sway/default.nix
@@ -37,12 +37,6 @@
   #    };
   #  };
   colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
-  qt = {
-    enable = true;
-    platformTheme = "gtk";
-    style.name = "adwaita-dark";
-    style.package = pkgs.adwaita-qt;
-  };
   dconf.settings = {
     "org/gnome/desktop/background" = {
       picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
@@ -51,15 +45,43 @@
       color-scheme = "prefer-dark";
     };
   };
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
+  };
+  # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 
   gtk = {
     enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
+    cursorTheme = {
+      package = pkgs.catppuccin-cursors.mochaDark;
+      name = "mochaDark";
     };
+    theme = {
+      name = "Catppuccin-Mocha-Compact-Sapphire-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["sapphire"];
+        size = "compact";
+        tweaks = ["rimless"];
+        variant = "mocha";
+      };
+    };
+    /*
+           iconTheme = {
+      package = pkgs.gruvboxPlus;
+      name = "GruvboxPlus";
+    };
+    */
   };
-
   # Wayland, X, etc. support for session vars
   systemd.user.sessionVariables = config.home-manager.users.tht.home.sessionVariables;
 }
