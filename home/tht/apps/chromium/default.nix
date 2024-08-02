dd0713d1a7ccd9f1df60b922c29ff2e74606a3ad
Bill Gates <me@theholytachanka.com>
2024-08-02 13:34:53 +0300

    added chromium

---
+++ home/tht/apps/chromium/default.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}: {
+  programs.chromium = {
+    enable = true;
+    package = pkgs.ungoogled-chromium;
+    extensions = [
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+    ];
+  };
+}
--- home/tht/default.nix
+++ home/tht/default.nix
@@ -16,6 +16,7 @@
     ./apps/spotify
     ./apps/qutebrowser
     ./apps/firefox
+    ./apps/chromium
     ../common/fish
     #./apps/yt-dlp
     ./apps/neovim
