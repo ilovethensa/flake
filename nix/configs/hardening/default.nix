17f0bb908249412c717c0acc569d9f2c1128fe2e
Bill Gates <me@theholytachanka.com>
2024-08-04 14:37:05 +0300

    fix?

--- nix/configs/hardening/default.nix
+++ nix/configs/hardening/default.nix
@@ -1,7 +1,7 @@
 {...}: {
   imports = [
     #./kernel.nix
-    ./systemd.nix
+    #./systemd.nix
   ];
   # Only allow members of the wheel group to execute sudo
   security.sudo.execWheelOnly = true;
