6629144926411ffb08f7c26dbe86be34f0a4bd9f
Bill Gates <me@theholytachanka.com>
2024-08-05 16:40:53 +0300

    now using lokinet

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -10,9 +10,23 @@
     inputs.home-manager.nixosModules.home-manager
   ];
   home-manager.extraSpecialArgs = {inherit inputs outputs;};
-  services.udisks2 = {
-    enable = true;
-    mountOnMedia = true;
+  services = {
+    flatpak.enable = true;
+    udisks2 = {
+      enable = true;
+      mountOnMedia = true;
+    };
+    lokinet = {
+      enable = true;
+      useLocally = true;
+      settings = {
+        dns = {
+          bind = "127.3.2.1";
+          upstream = ["193.110.81.0" "185.253.5.0"];
+        };
+
+        network.exit-node = ["exit.loki"];
+      };
+    };
   };
-  services.flatpak.enable = true;
 }
