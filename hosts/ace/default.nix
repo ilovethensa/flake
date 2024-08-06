74df7a7ae1a4ddc19b8212abe0997f3f5924f6cf
Bill Gates <me@theholytachanka.com>
2024-08-06 18:26:24 +0300

    fix

--- hosts/ace/default.nix
+++ hosts/ace/default.nix
@@ -11,24 +11,7 @@
   networking.hostName = "ace"; # Define hostname
 
   programs.fish.enable = true;
-  environment.persistence."/nix/persist" = {
-    hideMounts = true;
-    directories = [
-      "/home"
-      "/var/lib"
-      {
-        directory = "/root";
-        user = "root";
-      }
-    ];
-    files = [
-      #"/etc/machine-id"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-    ];
-  };
   # System state version
+  meta.persist = false;
   system.stateVersion = "23.05";
 }
