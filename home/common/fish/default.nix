8d639e66ea460d4bc0b2936afb83ee69d46220ab
TheHolyTachanka <me@theholytachanka.com>
2024-08-10 14:32:26 +0300

    fix

--- home/common/fish/default.nix
+++ home/common/fish/default.nix
@@ -73,7 +73,7 @@ in {
       ping = "${pkgs.gping}/bin/gping";
       ask = "${pkgs.tgpt}/bin/tgpt";
       ps = "${pkgs.procs}/bin/procs";
-      df = "${pkgs.duf}/bin/duf --hide local,network,fuse,special,loops";
+      df = "${pkgs.duf}/bin/duf";
       backup-server = "${backup-server}/bin/backup-server.sh";
       run-gui = "${run-gui}/bin/run-gui";
       reboot = "systemctl reboot";
