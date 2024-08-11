5fe7d41efdca7806dc7e8aa180029b0531c7a5dd
John <tht@pwned.page>
2024-08-11 16:33:29 +0300

    switched to flatpak obs

--- home/tht/default.nix
+++ home/tht/default.nix
@@ -92,19 +92,20 @@
         nvram = [ "/run/libvirt/nix-ovmf/AAVMF_CODE.fd:/run/libvirt/nix-ovmf/AAVMF_VARS.fd", "/run/libvirt/nix-ovmf/OVMF_CODE.fd:/run/libvirt/nix-ovmf/OVMF_VARS.fd" ]'';
     };
   };
-    programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-vkcapture
-      obs-pipewire-audio-capture
-      obs-vaapi
-      obs-gstreamer
-    ];
-  };
+  #  programs.obs-studio = {
+  #  enable = true;
+  #  plugins = with pkgs.obs-studio-plugins; [
+  #    wlrobs
+  #    obs-vkcapture
+  #    obs-pipewire-audio-capture
+  #    obs-vaapi
+  #    obs-gstreamer
+  #  ];
+  #};
   services.flatpak.packages = [
     "com.ultimaker.cura"
     "net.mkiol.SpeechNote"
+    "com.obsproject.Studio"
   ];
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
